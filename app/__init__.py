f7bb5f6f4972749d414ff827df969829d29c92ba
NewSuperChaos <55678394+NewSuperChaos@users.noreply.github.com>
2020-03-17 02:51:08 -0500

    Add files via upload
    draft of the front page

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from config import config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
 
 
-from app import routes, models
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    db.init_app(app)
+
+    # may add other codes later
+    return app
---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
