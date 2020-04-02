8f94566e77d40f37075be6bb9cc636bf038692e0
xiaohe.yu86@gmail.com <you@example.com>
2020-04-02 17:14:28 -0500

    pass defined image to ui

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, send_file
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
+import numpy as np
+import io
+from PIL import Image
+from app import HDX_Plots
 
 
 @app.route('/')
@@ -40,3 +44,125 @@ def ui():
 @app.route('/error')
 def error():
     return render_template('error.html')
+
+
+
+
+
+
+
+@app.route('/plotshow')
+def plotshow():
+    raw_data = [
+    [[255,255,255],[255,255,255],[255,255,255]],
+    [[0,0,1],[255,255,255],[0,0,0]],
+    [[255,255,255],[0,0,0],[255,255,255]],
+    [[100,50,23],[55,23,76],[157,75,32]]
+]
+    # my numpy array 
+    arr = np.array(raw_data)
+
+    # convert numpy array to PIL Image
+    img = Image.fromarray(arr.astype('uint8'))
+
+    # create file-object in memory
+    file_object = io.BytesIO()
+
+    # write PNG in file-object
+    img.save(file_object, 'PNG')
+
+    # move to beginning of file so `send_file()` it will read from start    
+    file_object.seek(0)
+
+    #return send_file(file_object, mimetype='image/png', as_attachment=True,cache_timeout=0,attachment_filename='HDX_Plot.png')
+    #return render_template('ui.html',user_image =file_object )
+
+    return send_file(file_object, mimetype='image/png', as_attachment=True,cache_timeout=0,attachment_filename='HDX_Plot.png')
+    
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
+
+
+
+# @app.route('/plotshow')
+# def plotshow():
+
+
+#     File_name = 'static/files/20191010_djb_H3H4dm.csv'
+#     csvFile = open(File_name, "r")
+#     reader = cs.reader(csvFile)
+#     # Set up time points and states
+#     Time_points1 = ['10', '100', '1000', '10000', '100000']
+#     States1 = ['H3H4dm', 'RV-H3H4dm']
+#     Proteins = ['H32_XENLA']
+#     Columns = []
+#     # Crate column to store data
+#     for Protein in Proteins:
+#         Columns.append(Protein)
+#         Columns.append(Protein + '_' + 'MaxUptake')
+#         for State in States1:
+            
+#             Columns.append(State)
+#             for Time in Time_points1:
+#                 Columns.append(Protein + '_' + State + '_' + Time)
+#                 Columns.append(Protein + '_' + State + '_' + Time + '_SD')
+#     Data1 = pd.DataFrame(columns=Columns)
+#     n, i = 0, 0
+#     Sequence = ''
+#     Sequence_number = ''
+#     # Read data form reader to Data
+#     for item in reader:
+#         if n != 0:
+#             if item[0] in Proteins:
+#                 if Sequence_number != item[1] + '-' + item[2]:
+#                     i += 1
+#                     Sequence_number = item[1] + '-' + item[2]
+#                     Data1.loc[i, item[0]] = Sequence_number
+#                     Data1.loc[i, item[0] + '_' + 'MaxUptake'] = item[6]
+#                 Time = str(int(float(item[9]) * 60 + 0.5))
+#                 State = item[8]
+#                 Protein = item[0]
+#                 if Time != '0':
+#                     Data1.loc[i, Protein + '_' + State + '_' + Time] = item[12]
+#                     Data1.loc[i, Protein + '_' + State + '_' + Time + '_SD'] = item[13]
+#         else:
+#             n = n + 1
+#     csvFile.close()
+#     # Save Data as csv file
+#     Data1.to_csv("For plot.csv", index=False, sep=',')
+
+#     K = HDX_Plots.heatmap(Data1, 'H32_XENLA', 'H3H4dm', 'RV-H3H4dm', Time_points1, rotation='H', max=5, step=10, color='rb', min=-5, step2=10,
+#                 file_name='FL_ASF1.eps')
+
+
+
+
+
+#     # create file-object in memory
+#     file_object = io.BytesIO()
+
+
+
+#     K.savefig(file_object, format='eps')
+
+
+#     # move to beginning of file so `send_file()` it will read from start    
+#     file_object.seek(0)
+
+#     #return send_file(file_object, mimetype='image/png', as_attachment=True,cache_timeout=0,attachment_filename='HDX_Plot.png')
+#     #return render_template('ui.html',user_image =file_object )
+
+#     return send_file(file_object, mimetype='application/postscript', as_attachment=True,cache_timeout=0,attachment_filename='HDX_Plot.eps')
+    
